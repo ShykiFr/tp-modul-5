4a45c5500affb072d8d8ccc37b0ff9751ac4396a
ShykiFr <moh.dzaki.ayatillah@gmail.com>
2026-03-29 22:14:11 +0700

    menambahkan class DataGeneric

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class DataGeneric<T>
+{
+    private T data;
+
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine($"Data yang tersimpan adalah: " + data);
+    }
+}
+
+public class Program
+{
+    public static void Main(String[] args)
+    {
+        DataGeneric<long> data = new DataGeneric<long>(103022400090);
+        data.PrintData();
+    }
+}
